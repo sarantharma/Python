8d620f510cdc08d93500d85604b82a2fcb544783
Saran <sarantharma7@gmail.com>
2021-06-04 13:52:40 -0400

    changes in buy_computer.py

--- Sequences/buy_computer.py
+++ Sequences/buy_computer.py
@@ -1,27 +1,28 @@
+available_parts = ["computer", "monitor", "keyboard", "mouse", "mouse mat", "hdmi cable", "dvd drive"]
 current_choice = "-"
 computer_parts = []
 
+# In one line copy things from one list to another
+# valid_choices = [str(i) for i in range(1, len(available_parts)+1)]
+
+valid_choices = []
+for i in range(1, len(available_parts)+1):
+    valid_choices.append(str(i))
+
+print(valid_choices)
+
+
 while current_choice != "0":
-    if current_choice in "12345":
+    if current_choice in valid_choices:
         print("Adding {}".format(current_choice))
-        if current_choice == "1":
-            computer_parts.append("computer")
-        elif current_choice == "2":
-            computer_parts.append("monitor")
-        elif current_choice == "3":
-            computer_parts.append("keyboard")
-        elif current_choice == "4":
-            computer_parts.append("mouse")
-        elif current_choice == "5":
-            computer_parts.append("mouse mat")
+        index = int(current_choice) - 1
+        chosen_part = available_parts[index]
+        computer_parts.append(chosen_part)
+        print("{} Added!".format(chosen_part))
     else:
         print("Please add options from the list below:")
-        print("1: computer")
-        print("2: monitor")
-        print("3: keyboard")
-        print("4: mouse")
-        print("5: mouse mat")
-        print("0 to finish!")
+        for part in available_parts:
+            print(" {0}: {1}".format(available_parts.index(part) + 1, part))
 
     current_choice = input()
 
